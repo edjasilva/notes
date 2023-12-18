85031786d602df993137f3f3c1c31d7f65504a7b
LAPTOP-QFELQHAM\tamar <tamaramonteiro2014@icloud.com>
2023-12-18 13:40:58 +0000

    mudei o formato da data

--- app/src/main/java/pt/iade/edjasilva/notes/NoteActivity.java
+++ app/src/main/java/pt/iade/edjasilva/notes/NoteActivity.java
@@ -7,13 +7,26 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.GregorianCalendar;
+
+import pt.iade.edjasilva.notes.models.NoteItem;
 
 public class NoteActivity extends AppCompatActivity {
-
+    protected EditText title_edit;
+    protected EditText notes_edit;
+    protected EditText date_text;
+    protected NoteItem item;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
+
+        item= new NoteItem(1,"sla", "sla", LocalDateTime.now(), LocalDateTime.now() );
 
         setupComponents();
     }
@@ -25,6 +38,9 @@ public class NoteActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_note, menu);
         return super.onPrepareOptionsMenu(menu);
     }
+
+
+
 
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
@@ -43,7 +59,24 @@ public class NoteActivity extends AppCompatActivity {
 
     private void setupComponents(){
         setSupportActionBar(findViewById(R.id.toolbar));
+        title_edit=(EditText) findViewById(R.id.title_edit);
+        notes_edit=(EditText) findViewById(R.id.notes_edit);
+        date_text=(EditText) findViewById(R.id.date_text);
+
+        populateView();
+    }
+    protected void populateView(){
+        title_edit.setText(item.getTitle());
+        notes_edit.setText(item.getContent());
+
+
+        // Formatando LocalDateTime para String usando DateTimeFormatter
+
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm:ss");
+        String formattedModifiedDate = item.getModifiedDate().format(formatter);
+
+        date_text.setText(formattedModifiedDate);
+
 
     }
-
 }
--- app/src/main/java/pt/iade/edjasilva/notes/models/NoteItem.java
+++ app/src/main/java/pt/iade/edjasilva/notes/models/NoteItem.java
@@ -1,14 +1,21 @@
-package pt.iade.edjasilva.notes;
+package pt.iade.edjasilva.notes.models;
+import java.time.LocalDateTime;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.GregorianCalendar;
 
 public class NoteItem {
+    private int id;
     private String title;
     private String content;
-    private GregorianCalendar creationDate;
-    private GregorianCalendar modifiedDate;
+    private LocalDateTime creationDate;
+    private LocalDateTime modifiedDate;
 
-
-    public NoteItem(String title, String content, GregorianCalendar creationDate, GregorianCalendar modifiedDate){
+public NoteItem(){
+    this(0, "", "", LocalDateTime.now(), LocalDateTime.now());
+}
+    public NoteItem(int id, String title, String content, LocalDateTime creationDate, LocalDateTime modifiedDate){
+        this.id=id;
         this.title=title;
         this.content=content;
         this.creationDate=creationDate;
@@ -19,15 +26,18 @@ public class NoteItem {
         return title;
     }
 
+    public int getId(){
+        return id;
+    }
     public String getContent(){
         return content;
     }
 
-    public GregorianCalendar getCreationDate(){
+    public LocalDateTime getCreationDate(){
         return creationDate;
     }
 
-    public GregorianCalendar getModifiedDate(){
+    public LocalDateTime getModifiedDate(){
         return modifiedDate;
     }
 
@@ -36,15 +46,16 @@ public class NoteItem {
         this.title=title;
     }
 
+
     public void setContent(String content){
         this.content=content;
     }
 
-    public void setCreationDate(GregorianCalendar creationDate){
+    public void setCreationDate(LocalDateTime creationDate){
         this.creationDate=creationDate;
     }
 
-    public void setModifiedDate(GregorianCalendar modifiedDate){
+    public void setModifiedDate(LocalDateTime modifiedDate){
         this.modifiedDate=modifiedDate;
     }
 }
