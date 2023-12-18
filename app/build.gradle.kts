ee81c4b1aa7ab93ecccff149444029dfa1dcbd0b
LAPTOP-QFELQHAM\tamar <tamaramonteiro2014@icloud.com>
2023-12-18 02:35:29 +0000

    h

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "pt.iade.edjasilva.notes"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "pt.iade.edjasilva.notes"
--- app/src/main/java/pt/iade/edjasilva/notes/MainActivity.java
+++ app/src/main/java/pt/iade/edjasilva/notes/MainActivity.java
@@ -3,8 +3,12 @@ package pt.iade.edjasilva.notes;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.view.Menu;
+
 import android.os.Bundle;
 import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +16,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        setupComponents();
     }
 
+    @Override
+    public boolean onPrepareOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return super.onPrepareOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.action_new_note){
+            Intent intent= new Intent(MainActivity.this,NoteActivity.class);
+            startActivity(intent);
+
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void setupComponents(){
+        setSupportActionBar(findViewById(R.id.toolbar));
+
+    }
 }
